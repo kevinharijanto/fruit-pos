d1ea9a98aad4c2afc10ee38a86ff4d8852d4f054
unknown <kevinhari777@gmail.com>
2026-02-23 00:44:06 +0700

    fix bug

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -6,12 +6,8 @@ export default function LoginLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="id" className="h-full">
-      <body className="min-h-svh bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-100 antialiased">
-        <DarkModeProvider>
-          {children}
-        </DarkModeProvider>
-      </body>
-    </html>
+    <DarkModeProvider>
+      {children}
+    </DarkModeProvider>
   );
 }
